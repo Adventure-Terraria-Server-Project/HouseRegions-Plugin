ff52830d4586e68818a88f9105e693df41cf77ef
CoderCow <CoderCow@gmx.de>
2016-09-16 02:41:47 +0200

    Version tick 1.24. Now depends on Common Lib 3.5 and Terraria 1.3.3.2.

--- Implementation/HouseRegionsPlugin.cs
+++ Implementation/HouseRegionsPlugin.cs
@@ -11,7 +11,7 @@ using TerrariaApi.Server;
 using TShockAPI;
 
 namespace Terraria.Plugins.CoderCow.HouseRegions {
-  [ApiVersion(1, 23)]
+  [ApiVersion(1, 24)]
   public class HouseRegionsPlugin: TerrariaPlugin {
     private const string TracePrefix = @"[Housing] ";
     public const string Define_Permission          = "houseregions.define";
