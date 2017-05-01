596d214caa183c123fecc8776c17d4a24ffc820b
CoderCow <CoderCow@gmx.de>
2017-05-02 01:49:52 +0200

    chore(version): tick plugin version 1.2.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,6 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+// [assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.2.0+8.Branch.master.Sha.352e9a29b5b4c44b6afd6b1972fd3cb4aad473c5")]
